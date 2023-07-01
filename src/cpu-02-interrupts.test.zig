ff5fbd68616569da3314574b38c8a204181acee9
Louis Acresti <louis.acresti@gmail.com>
2023-07-01 11:53:00 -0700

    document interrupt issue

--- src/cpu-02-interrupts.test.zig
+++ src/cpu-02-interrupts.test.zig
@@ -25,6 +25,7 @@ pub fn run() !void {
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
 
+    var readBuf: [1024]u8 = undefined;
     var expectedBuf: [1024]u8 = undefined;
     var actualBuf: [1024]u8 = undefined;
 
@@ -39,21 +40,27 @@ pub fn run() !void {
     while (cpu.pc != 0x0101) {
         cpu.step();
     }
-    var line_number: u64 = 0;
+
+    var line_number: u64 = 1;
 
     // Now start comparing our log:
-    while (try in_stream.readUntilDelimiterOrEof(&expectedBuf, '\n')) |expected| {
-        while (line_number == 152480 and cpu.halted) {
-            // std.debug.print("waiting...\n", .{});
+    while (try in_stream.readUntilDelimiterOrEof(&readBuf, '\n')) |expected| {
+        // It's possible our timer isn't correctly implemented, because I can't
+        // get the logs to align here after this line while the CPU is halted.
+        //
+        // This could have something to do with how the log file was generated;
+        // specifically, when does it log while the CPU is halted?
+        while (line_number == 152481 and cpu.halted) {
             cpu.step();
         }
-        // std.debug.print("{} {s}\n", .{ line_number, expected });
-        try std.testing.expectEqualStrings(expected, try std.fmt.bufPrint(&actualBuf, "{}", .{cpu}));
+        const expected_str = try std.fmt.bufPrint(&expectedBuf, "{}: {s}", .{ line_number, expected });
+        const actual_str = try std.fmt.bufPrint(&actualBuf, "{}: {s}", .{ line_number, cpu });
+        try std.testing.expectEqualStrings(expected_str, actual_str);
         cpu.step();
         line_number += 1;
     }
 }
 
-// test "blargg 02-interrupts log comparison" {
-//     try run();
-// }
+test "blargg 02-interrupts log comparison" {
+    try run();
+}
